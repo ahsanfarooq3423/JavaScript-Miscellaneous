145a957c9d18a3ea98ebe3246c3b907adde3b7e6
Ahsan <ahsanfarooq3423@gmail.com>
2019-10-14 21:15:12 +0500

    Layout Functionality Completed

--- burger-app-react/src/components/Logo/Logo.js
+++ burger-app-react/src/components/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
+import { tsPropertySignature } from '@babel/types';
 
 const logo = () => (
-    <div className  = {classes.Logo}>
+    <div className  = {classes.Logo} >
         <img src = {burgerLogo} alt = "MyBurger"/>
     </div>
 );
