0189512d2a75acb84e7319a7d20140cc59ce2559
Ahsan <ahsanfarooq3423@gmail.com>
2019-10-24 20:11:02 +0500

    withErrorHandler

--- burger-app-react/src/axios-orders.js
+++ burger-app-react/src/axios-orders.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
-});
+    baseURL : 'https://react-my-burger-3670c.firebaseio.com/'
+})
 
 export default instance;
--- burger-app-react/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app-react/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -84,29 +85,30 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        this.setState( { loading: true } );
+        //alert('You continue!');
+        //the order to sent to the to database
+        this.setState({loading : true})
         const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max Schwarzmüller',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '41351',
-                    country: 'Germany'
+            ingredients : this.state.ingredients,
+            price : this.state.totalPrice,
+            customer : {
+                name: 'Ahsan Farooq',
+                address : {
+                    street : 'StreetTest 2',
+                    zipCode : '2342',
+                    country : 'Pakistan'
                 },
-                email: 'test@test.com'
+                email : 'ahsan@test.com'
             },
             deliveryMethod: 'fastest'
         }
-        // axios.post( '/orders.json', order )
-        //     .then( response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     } )
-        //     .catch( error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     } );
+        axios.post('/orders.json',order)
+            .then(response => {
+                this.setState({loading : false, purchasing : false})
+            }) 
+            .catch(error => {
+                this.setState({loading : false, purchasing : false})
+            });
     }
 
     render () {
@@ -143,4 +145,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
