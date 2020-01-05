af7d9a88894cfaee9ac1c0399396654a80d31460
Ahsan <ahsanfarooq3423@gmail.com>
2020-01-05 13:25:54 +0500

    react hooks basics

---
+++ react-hooks/src/components/Todo.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+
+const todo = props => {
+
+    const inputState = useState('');
+
+    const inputChangeHandler = (event) => {
+        inputState[1](event.target.value)
+    }
+
+    return (
+        <React.Fragment>
+            <input 
+                type = 'text' 
+                placeholder = 'Todo'
+                value = {inputState[0]}
+                onChange = {inputChangeHandler} />
+            <button type = "button">Add</button>
+            <ul>
+                <li>
+                {inputState[0]}
+                </li>
+            </ul>
+        </React.Fragment>
+    )
+   
+}
+
+export default todo;
